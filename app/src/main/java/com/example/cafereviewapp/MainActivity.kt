ec4c1062d54629a9262d48c170fa9e12e5d50010
pyomin <hpmfool@naver.com>
2021-02-04 16:01:07 +0900

    메인페이지 수정

--- app/src/main/java/com/example/cafereviewapp/MainActivity.kt
+++ app/src/main/java/com/example/cafereviewapp/MainActivity.kt
@@ -1,14 +1,50 @@
 package com.example.cafereviewapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var bookcafe : Button
+    lateinit var catcafe : Button
+    lateinit var cafe : Button
+    lateinit var flowercafe : Button
+    lateinit var studycafe : Button
+    lateinit var gamecafe : Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bookcafe = findViewById(R.id.bookcafe)
+        catcafe = findViewById(R.id.catcafe)
+        cafe = findViewById(R.id.cafe)
+        flowercafe = findViewById(R.id.flowercafe)
+        studycafe = findViewById(R.id.studycafe)
+        gamecafe = findViewById(R.id.gamecafe)
+
+
+        studycafe.setOnClickListener {
+            val intent = Intent(this, studyActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+
+        cafe.setOnClickListener {
+            val intent = Intent(this, dessertActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+
 
 
     }
---
+++ app/src/main/java/com/example/cafereviewapp/dessertActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cafereviewapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dessertActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dessert)
+    }
+}
