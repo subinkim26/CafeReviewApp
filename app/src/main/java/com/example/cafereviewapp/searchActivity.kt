16e5d77d990ef626dc6541b26983d6ce13652c6d
pyomin <hpmfool@naver.com>
2021-02-10 15:22:43 +0900

    진짜진짜 최종

--- app/src/main/java/com/example/cafereviewapp/searchActivity.kt
+++ app/src/main/java/com/example/cafereviewapp/searchActivity.kt
@@ -1,11 +1,28 @@
 package com.example.cafereviewapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class searchActivity : AppCompatActivity() {
+
+
+    lateinit var secondButton : ImageButton
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        secondButton = findViewById(R.id.secondButton)
+
+
+        secondButton.setOnClickListener {
+            val intent = Intent(this, searchDetailActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
